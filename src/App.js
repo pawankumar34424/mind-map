0cc686d7b7e84c9c57aa613f7de757721321400e
Pawan <pawan@localhost.com>
2020-09-04 23:39:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MindElixir, { E } from 'mind-elixir'
+
+
+function App() {
+
+  const [mindElixir, setMindElixir] = useState();
+  const [selectedNodeId, setSelectedNodeId] = useState('root');
+  
+  useEffect(() => {
+    const _mindElixir = new MindElixir({
+      el: "#map",
+      direction: MindElixir.LEFT,
+      data: MindElixir.new("new topic"),
+      draggable: false, // default true
+      contextMenu: false, // default true
+      toolBar: false, // default true
+      nodeMenu: false, // default true
+      keypress: false // default true
+    });
+    _mindElixir.init();
+
+    _mindElixir.bus.addListener('selectNode', node => {
+      setSelectedNodeId(node.id)
+    })
+
+    _mindElixir.bus.addListener('operation', operation => {
+      console.log(operation)
+      /*if(operation.name==='beginEdit'){
+        operation.obj['style']= {color: "#ecf0f1", fontSize: "24", background: "#2980b9"}
+      }*/
+      // return {
+      //   name: action name,
+      //   obj: target object
+      // }
+    
+      // name: [insertSibling|addChild|removeNode|beginEdit|finishEdit]
+      // obj: target
+    
+      // name: moveNode
+      // obj: {from:target1,to:target2}
+    })
+    _mindElixir.initSide()
+    setMindElixir(_mindElixir)
+  }, []);
+
+  const addNewChild=()=>{
+    const selectedNode=E(selectedNodeId);
+    mindElixir.addChild(selectedNode)
+    console.log(mindElixir,mindElixir.getAllData())
+    const { history } = mindElixir;
+    const { obj } = history[history.length-1];
+    obj['style']= {color: "#ecf0f1", fontSize: "24", background: "#2980b9"};
+    mindElixir.updateNodeStyle(obj);
+  }
+ 
+  const deleteSelected=()=>{
+    const selectedNode=E(selectedNodeId);
+    mindElixir.removeNode(selectedNode)
+    setSelectedNodeId(null)
+  }
+  return (
+    <div className="App">
+        <button onClick={()=>addNewChild()}>Add New Child</button>
+        <button onClick={()=>mindElixir.toCenter()}>Reset View</button>
+        <button 
+            disabled={!selectedNodeId || selectedNodeId==='root'} 
+            onClick={()=>deleteSelected()}>Delete</button>
+       <div id="map" style={{ height: "500px", width: "80%" }}> </div>
+    </div>
+  );
+}
+
+export default App;
